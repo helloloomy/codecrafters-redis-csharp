82783b62597cf8bb8fd5228c4afe58eac70e401e
Chris Nightingale <wooffet@gmail.com>
2023-04-23 17:00:11 +0100

    revert last commit

--- src/Server.cs
+++ src/Server.cs
@@ -11,7 +11,7 @@ while (handler.Connected)
 {
     try
     {
-        var stream = handler.GetStream();
+        await using var stream = handler.GetStream();
 
         var msg = "+PONG\r\n";
         var msgBytes = Encoding.UTF8.GetBytes(msg);
