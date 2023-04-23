d4005f41f9cf7c94ea981133a080bacf0c183732
Chris Nightingale <wooffet@gmail.com>
2023-04-23 16:10:08 +0100

    respond to multiple pings

--- src/Server.cs
+++ src/Server.cs
@@ -2,9 +2,6 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.WriteLine("Logs from your program will appear here!");
-
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 
 try
