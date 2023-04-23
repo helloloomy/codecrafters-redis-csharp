59ce8fda4d6f17d92421c29d9e4ef9d0aa26404b
Chris Nightingale <wooffet@gmail.com>
2023-04-23 16:13:19 +0100

    use while loop to keep server open after handling first message

--- src/Server.cs
+++ src/Server.cs
@@ -7,14 +7,17 @@ TcpListener server = new TcpListener(IPAddress.Any, 6379);
 try
 {
     server.Start();
-    using var handler = await server.AcceptTcpClientAsync();
-    await using var stream = handler.GetStream();
+    while (true)
+    {
+        using var handler = await server.AcceptTcpClientAsync();
+        await using var stream = handler.GetStream();
 
-    var msg = "+PONG\r\n";
-    var msgBytes = Encoding.UTF8.GetBytes(msg);
-    await stream.WriteAsync(msgBytes);
+        var msg = "+PONG\r\n";
+        var msgBytes = Encoding.UTF8.GetBytes(msg);
+        await stream.WriteAsync(msgBytes);
 
-    Console.WriteLine("Sent PONG response!");
+        Console.WriteLine("Sent PONG response!");
+    }
 }
 finally
 {
